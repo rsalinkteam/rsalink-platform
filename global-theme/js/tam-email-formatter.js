626a72a8f13d39dbe680efca86adbde9a2feb765
Victor Laucas <victorlaucas@gmail.com>
2019-05-16 14:17:06 -0600

    Restored to a previous version of tam email formatter and removed debugging console.log statements

--- global-theme/js/tam-email-formatter.js
+++ global-theme/js/tam-email-formatter.js
@@ -5,37 +5,28 @@
 //  Last Modified:  16 May 2019
 //  Description:    Formats documents created from TAM emails in 360 pages
 function byeP() {
-    console.log("Starting byeP formatting email");
     const pTags = document.getElementsByClassName("jive-rendered-content")[0].getElementsByTagName("p");
     for(ptag of pTags) {
         if(ptag.style.minHeight == "8pt") {
             ptag.remove();
         }
     }
-    console.log("Removed weird styling from p tags");
 }
 
 function remove_email_garbage() {
-    console.log("Starting remove_email function");
     const pTags = document.getElementsByClassName("jive-rendered-content")[0].getElementsByTagName("p");
     const regex = new RegExp("<span>&lt;[^\s].*?&gt;</span>");
-
     for(ptag of pTags) {
         ptag.innerHTML = ptag.innerHTML.replace(regex, "");
     }
-    console.log("Regex removed the garbage")
-
 }
 
-
-    console.log("Event listener load triggered");
-    
+window.addEventListener("load", function() {
     const browseIdThreeSixty = [202388,205761,207978,207979,207980,207982,207983,207984,207985,207987,207988,207989,207990,207991,207992,207993,207994,207995,207996,207997,207998,207999,211487,211850,214468,215603,221449,231606,232132,232134,232136,207989,207990,207991,207992,207993,207994,207995,207996,207997,207998,207999,211487,211850,214468,215603,221449,231606,232132,232134,232136];
     if(browseIdThreeSixty.indexOf(Number(placeID)) > -1) {
         console.log('Identified browse id ', placeID, " as a 360 page");
         remove_email_garbage();
         const pTags = document.getElementsByClassName("jive-rendered-content")[0].getElementsByTagName("p");
-        console.log(pTags);
 
         let looping = true;
         while(looping) {
@@ -45,8 +36,8 @@ function remove_email_garbage() {
                 }
                 else {
                     looping = false;
-                    console.log("Looping set to false, script ended.")
                 }
             }
         }
     }
+});
