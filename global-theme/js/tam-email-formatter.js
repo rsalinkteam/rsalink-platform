f3f3c0bf2ed807f8f55a26d925c54abf0e107f47
Victor Laucas <victorlaucas@gmail.com>
2019-06-10 12:39:46 -0600

    Added validation to check length of ptags array tam-email-formatter.js

--- global-theme/js/tam-email-formatter.js
+++ global-theme/js/tam-email-formatter.js
@@ -25,18 +25,27 @@ function remove_email_garbage() {
 const browseIdThreeSixty = [202388,205761,207978,207979,207980,207982,207983,207984,207985,207987,207988,207989,207990,207991,207992,207993,207994,207995,207996,207997,207998,207999,211487,211850,214468,215603,221449,231606,232132,232134,232136,207989,207990,207991,207992,207993,207994,207995,207996,207997,207998,207999,211487,211850,214468,215603,221449,231606,232132,232134,232136];
 if(browseIdThreeSixty.indexOf(Number(placeID)) > -1) {
     console.log('Identified browse id ', placeID, " as a 360 page");
-    remove_email_garbage();
+    
+    
     
     const pTags = document.getElementsByClassName("jive-rendered-content")[0].getElementsByTagName("p");
-    let looping = true;
-    while(looping) {
-        for(ptag of pTags) {
-            if(ptag.style.minHeight != "") {
-                byeP();
-            }
-            else {
-                looping = false;
+    if(pTags.length > 0) {
+        let looping = true;
+
+        while(looping) {
+            for(ptag of pTags) {
+                if(ptag.style.minHeight != "") {
+                    byeP();
+                }
+                else {
+                    looping = false;
+                }
             }
         }
+
+        remove_email_garbage();
+    } else {
+        console.log("There are no p tags with the class name of jive-rendered-content. End of script");
     }
+        
 }
