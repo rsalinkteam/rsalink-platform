dcbfda71a9809522c86906ceb1d0db0b5d138204
Santhosh V <santhosh_v.v@rsa.com>
2019-10-22 09:55:06 +0530

    Updated the rsalink-announcement JS file to prioritize system level announcements over space level announcements
    Updated the rsalink-announcement.js file to prioritize system level announcements over space level announcements related to RSALINK-8110

--- global-theme/js/rsalink-announcement.js
+++ global-theme/js/rsalink-announcement.js
@@ -2,15 +2,27 @@
 //  Location:       Website Header
 //  Author:         Santhosh V
 //  Last Editor:    Santhosh V
-//  Last Modified:  20 Dec 2018
+//  Last Modified:  22 Oct 2019
 //  Description:    The script in this file gets the home page announcements and display same announcements in other pages.
 var jq = jQuery.noConflict();
-var url = 'https://community.rsa.com';
 jq(document).ready(function() {
-	jq('<div id="announcementContainer"></div>').insertBefore("#jive-widget-container");
-	if(document.getElementById("jive-alert")!= null){
-	  jq('#announcementContainer').hide();
-	}else{
-	  jq('#announcementContainer').load(url+' #jive-alert');
-	}
+	jq('#jive-alert').hide();
+	setTimeout(function(){
+		var url = 'https://community.rsa.com';
+		var container = jq('<div id="announcementContainer"></div>').insertBefore("#jive-widget-container");
+		var currentUrl = window.location.href;
+		if(currentUrl !== url){
+			jq(container).load(url+' #jive-alert', function(){
+			    if(this.innerHTML != ""){
+					jq('#announcementContainer #jive-alert').show();
+					jq('#announcementContainer #jive-alert #jive-alert-1').removeClass('jive-alert-type');
+					jq('#announcementContainer #jive-alert').css('margin-bottom','10px');
+			    }else{
+			    	jq('#jive-alert').show();
+			    }
+			});
+		}else{
+			jq('#jive-alert').show();
+		}
+	}, 50);
 });
