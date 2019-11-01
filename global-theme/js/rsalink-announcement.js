6b0281769a9bf371a21be6edf84d48968c394bb8
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2019-11-01 09:45:52 -0600

    Switched from tabs to spaces in rsalink-announcement.js and updated themeBuilder_global_header_mainContent.htm to make sure it matches what is live.

--- global-theme/js/rsalink-announcement.js
+++ global-theme/js/rsalink-announcement.js
@@ -4,32 +4,32 @@
 //  Author:         Santhosh V
 //  Last Editor:    Jeff Shurtliff
 //  Last Modified:  01 Nov 2019
-//  Version:		1.0.2
+//  Version:        1.0.3
 
 var jq = jQuery.noConflict();
 jq(document).ready(function() {
-	jq('#jive-alert').hide();
-	setTimeout(function(){
-		var url = 'https://community.rsa.com';
-		var homePage = "1002";
-		if (placeID == homePage) {
-			var container = jq('<div id="announcementContainer"></div>').insertBefore("#jive-body");
-		} else {
-			var container = jq('<div id="announcementContainer"></div>').insertBefore("#jive-widget-container");
-		}
-		var currentUrl = window.location.href;
-		if(currentUrl !== url){
-			jq(container).load(url+' #jive-alert', function(){
-				if(this.innerHTML != ""){
-					jq('#announcementContainer #jive-alert').show();
-					jq('#announcementContainer #jive-alert #jive-alert-1').removeClass('jive-alert-type');
-					jq('#announcementContainer #jive-alert').css('margin-bottom','10px');
-				}else{
-					jq('#jive-alert').show();
-				}
-			});
-		}else{
-			jq('#jive-alert').show();
-		}
-	}, 50);
+    jq('#jive-alert').hide();
+    setTimeout(function(){
+        var url = 'https://community.rsa.com';
+        var homePage = "1002";
+        if (placeID == homePage) {
+            var container = jq('<div id="announcementContainer"></div>').insertBefore("#jive-body");
+        } else {
+            var container = jq('<div id="announcementContainer"></div>').insertBefore("#jive-widget-container");
+        }
+        var currentUrl = window.location.href;
+        if(currentUrl !== url){
+            jq(container).load(url+' #jive-alert', function(){
+                if(this.innerHTML != ""){
+                    jq('#announcementContainer #jive-alert').show();
+                    jq('#announcementContainer #jive-alert #jive-alert-1').removeClass('jive-alert-type');
+                    jq('#announcementContainer #jive-alert').css('margin-bottom','10px');
+                }else{
+                    jq('#jive-alert').show();
+                }
+            });
+        }else{
+            jq('#jive-alert').show();
+        }
+    }, 50);
 });
