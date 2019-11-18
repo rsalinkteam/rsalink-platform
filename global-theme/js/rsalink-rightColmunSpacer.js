b04c9be61bbcc2a1c2040f3cd63661124e9e9887
Santhosh V <santhosh_v.v@rsa.com>
2019-11-18 14:58:04 +0530

    Added a fix to rightColumnSpacer.js
    Fixed the right column overlapping issue in rightColumnSpacer.js part of RSALINK-8409

--- global-theme/js/rsalink-rightColmunSpacer.js
+++ global-theme/js/rsalink-rightColmunSpacer.js
@@ -2,15 +2,22 @@
 //  Location:       Website Header
 //  Author:         Santhosh V
 //  Last Editor:    Santhosh V
-//  Last Modified:  10 Apr 2019
+//  Last Modified:  18 Nov 2019
 //  Description:    The script in this file adjusts the height of the right column to match the left column's height.
 
 var jq = jQuery.noConflict();
 jq(window).load(function(){
+	setTimeout(function(){
+		rightColumn();
+ 	}, 2000);
+});
+function rightColumn(){
 	var containerHeight1 = jq('.jive-body-layout-l .jive-widget-container-large').height();
 	var containerHeight2 = jq('.jive-body-layout-s .jive-widget-container-small').height();
 	var containerHeight3 = jq('.jive-body-layout-l2 .jive-widget-container-large').height();
 	var containerHeight4 = jq('.jive-body-layout-s1 .jive-widget-container-small').height();
+	var containerHeight5 = jq('.jive-body-layout-s2 .jive-widget-container-small').height();
+	var containerHeight6 = jq('.jive-body-layout-s3 .jive-widget-container-small').height();
 	if(containerHeight2 > containerHeight1){
 		jq('.jive-body-layout-s .jive-widget-container-small').height(containerHeight2 +'px');
 	}else{
@@ -21,4 +28,9 @@ jq(window).load(function(){
 	}else{
 		jq('.jive-body-layout-s1 .jive-widget-container-small').height(containerHeight3 +'px');
 	}
-});
+	if(containerHeight6 > containerHeight5){
+		jq('.jive-body-layout-s3 .jive-widget-container-small').height(containerHeight6 +'px');
+	}else{
+		jq('.jive-body-layout-s3 .jive-widget-container-small').height(containerHeight5 +'px');
+	}
+}
