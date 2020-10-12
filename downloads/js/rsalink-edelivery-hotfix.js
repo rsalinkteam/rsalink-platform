a025d3329fbea6e809f3060ce94f456e80ad14ba
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2020-10-12 13:40:50 -0600

    Updated rsalink-edelivery-hotfix.js to only attempt replacements when http links are found

--- downloads/js/rsalink-edelivery-hotfix.js
+++ downloads/js/rsalink-edelivery-hotfix.js
@@ -3,8 +3,8 @@
 //  Location:       Downloads Documents
 //  Author:         Jeff Shurtliff
 //  Last Editor:    Jeff Shurtliff
-//  Last Modified:  05 Oct 2020
-//  Version:        1.0.0
+//  Last Modified:  12 Oct 2020
+//  Version:        1.1.0
 
 var jq = jQuery.noConflict();
 jq(document).ready(function() {
@@ -13,9 +13,14 @@ jq(document).ready(function() {
     if (location.href.includes('docs/DOC')) {
       console.log('A downloads document has been identified.');
       jq('.jive-link-external-small').each(function() {
-        var updatedLink = jq(this).prop('href').replace('url=http%3A', 'url=https%3A');
-        jq(this).prop("href", updatedLink);
-        console.log("Updated a download hyperlink to use HTTPS.")
+        var origLink = jq(this).prop('href');
+        var pattern = /http%3A/;
+        var notSecure = pattern.test(origLink);
+        if (notSecure) {
+          var updatedLink = jq(this).prop('href').replace('url=http%3A', 'url=https%3A');
+          jq(this).prop("href", updatedLink);
+          console.log("Updated a download hyperlink to use HTTPS.");
+        }
       });
     }
   }
